660ea3da9906bf7684d8078a36627a536c0ff098
Anton Kovalyov <keldzh@gmail.com>
2022-12-03 01:04:58 +0300

    Advent Of Code 2022. Day 2: Rock Paper Scissors. Part 1

--- Main.dart
+++ Main.dart
@@ -1,23 +1,82 @@
 import 'dart:io';
 
-import 'package:collection/collection.dart';
-
 void main() {
   final input = File("input");
   final maxCalories = input.readAsLinesSync()
-      .fold([<int>[]], splitBags)
-      .map((e) => e.reduce((value, element) => value + element))
-      .sorted((a, b) => a.compareTo(b) * -1)
-      .take(3)
-      .sum;
+      .map((e) => Round(e))
+      .fold(0, (acc, round) => acc + round.score());
   print(maxCalories);
 }
 
-List<List<int>> splitBags(List<List<int>> acc, String value) {
-  if (value.isEmpty) {
-    acc.add([]);
-  } else {
-    acc.last.add(int.parse(value));
+abstract class Shape extends Comparable<Shape> {
+  int get score;
+
+  factory Shape.fromInt(int shapeType) {
+    switch (shapeType) {
+      case 0: return Rock();
+      case 1: return Paper();
+      case 2: return Scissors();
+      default: throw ArgumentError("unknown shape type: $shapeType");
+    }
   }
-  return acc;
+}
+
+class Rock implements Shape {
+  @override
+  int get score => 1;
+
+  @override
+  int compareTo(Shape other) {
+    if (other is Scissors) return 1;
+    if (other is Paper) return -1;
+    if (other is Rock) return 0;
+    throw ArgumentError("Unknown class ${other.runtimeType.toString()}");
+  }
+}
+
+class Paper implements Shape {
+  @override
+  int get score => 2;
+
+  @override
+  int compareTo(Shape other) {
+    if (other is Rock) return 1;
+    if (other is Scissors) return -1;
+    if (other is Paper) return 0;
+    throw ArgumentError("Unknown class ${other.runtimeType.toString()}");
+  }
+}
+
+class Scissors implements Shape {
+  @override
+  int get score => 3;
+
+  @override
+  int compareTo(Shape other) {
+    if (other is Paper) return 1;
+    if (other is Rock) return -1;
+    if (other is Scissors) return 0;
+    throw ArgumentError("Unknown class ${other.runtimeType.toString()}");
+  }
+}
+
+class Round {
+  Shape _otherShape;
+  Shape _myShape;
+
+  Round(String roundData) {
+    _otherShape = Shape.fromInt(roundData.codeUnitAt(0) - "A".codeUnitAt(0));
+    _myShape = Shape.fromInt(roundData.codeUnitAt(2) - "X".codeUnitAt(0));
+  }
+
+  int score() {
+    var result = _myShape.score;
+    final compareResult = _myShape.compareTo(_otherShape);
+    if (compareResult == 0) {
+      result += 3;
+    } else if (compareResult > 0) {
+      result += 6;
+    }
+    return result;
+  }
 }
