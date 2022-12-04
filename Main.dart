7beb21a668e3631021255213410869730e3f62d8
Anton Kovalyov <keldzh@gmail.com>
2022-12-04 15:24:20 +0300

    Advent Of Code 2022. Day 4: Camp Cleanup. Part 1

--- Main.dart
+++ Main.dart
@@ -1,38 +1,81 @@
 import 'dart:io';
 
-import 'package:collection/collection.dart';
-
 void main() {
   final input = File("input");
-  final priorities = input
+  final fullyContainedSum = input
       .readAsLinesSync()
-      .slices(3)
-      .map((groupRucksacks) => groupRucksacks.map((rucksack) => rucksack.toSet()))
-      .map((groupRucksacks) {
-        var result = groupRucksacks.first;
-        groupRucksacks.skip(1).forEach((rucksack) {
-          result = result.intersection(rucksack);
-        });
-        return result;
+      .map((groupAssignments) {
+        final ranges = groupAssignments.split(",")
+            .map((assignment) => assignment.trim());
+        return Pair(ranges.first, ranges.last);
       })
-      .map((e) => e.single.codeUnitAt(0))
-      .map((e) {
-        if ("a".codeUnitAt(0) <= e && e <= "z".codeUnitAt(0)) {
-          return e - "a".codeUnitAt(0) + 1;
-        } else {
-          return 27 + e - "A".codeUnitAt(0);
-        }
-      })
-      .sum;
-  print(priorities);
+      .map((groupAssignments) =>
+        Pair(
+          IntRange.fromStringPeriod(groupAssignments.first),
+          IntRange.fromStringPeriod(groupAssignments.second)
+        )
+      )
+      .where((groupRanges) =>
+          groupRanges.first.isFullyContains(groupRanges.second) ||
+          groupRanges.second.isFullyContains(groupRanges.first)
+      )
+      .length;
+  print(fullyContainedSum);
 }
 
-extension StringExtenstions on String {
-  Set<String> toSet() {
-    final result = <String>{};
-    for (var i = 0; i < length; ++i) {
-      result.add(this[i]);
-    }
-    return result;
+class Pair<T> {
+  final T first;
+  final T second;
+
+  Pair(T this.first, T this.second);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Pair &&
+              runtimeType == other.runtimeType &&
+              first == other.first &&
+              second == other.second;
+
+  @override
+  int get hashCode => first.hashCode ^ second.hashCode;
+
+  @override
+  String toString() {
+    return 'Pair{first: $first, second: $second}';
+  }
+}
+
+class IntRange {
+  final int first;
+  final int last;
+
+  IntRange(this.first, this.last) {
+    assert(first <= last);
+  }
+
+  factory IntRange.fromStringPeriod(String period) {
+    final values = period.split("-");
+    return IntRange(int.parse(values.first), int.parse(values.last));
+  }
+
+  bool isFullyContains(IntRange other) {
+    return first <= other.first && other.last <= last;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is IntRange &&
+          runtimeType == other.runtimeType &&
+          first == other.first &&
+          last == other.last;
+
+  @override
+  int get hashCode => first.hashCode ^ last.hashCode;
+
+  @override
+  String toString() {
+    return 'IntRange{first: $first, last: $last}';
   }
 }
