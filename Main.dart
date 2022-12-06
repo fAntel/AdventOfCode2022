afef30fb4f282122d27e398a26e4805e2333c702
Anton Kovalyov <keldzh@gmail.com>
2022-12-07 01:17:45 +0300

    Advent Of Code 2022. Day 6: Tuning Trouble. Part 2

--- Main.dart
+++ Main.dart
@@ -9,31 +9,39 @@ void main() async {
       return;
     }
 
-    final buffer = <int>[];
-    buffer.addAll(await file.read(3));
+    int packetBeginning = await findBeginning(file, 4);
+    int messageBeginning = await findBeginning(file, 14) + packetBeginning;
 
-    int i = 3;
-    int b;
-    int removed;
-    final notUniqueElements = <int>[];
-    while ((b = file.readByteSync()) >= 0) {
-      ++i;
-      if (buffer.contains(b)) {
-        notUniqueElements.add(b);
-      }
-      buffer.add(b);
-      if (notUniqueElements.isEmpty) {
-        break;
-      } else {
-        removed = buffer.removeAt(0);
-        notUniqueElements.remove(removed);
-      }
-    }
-
-    print(i);
+    print(messageBeginning);
   } catch (e) {
     print("Error: $e");
   } finally {
     file.closeSync();
   }
+}
+
+Future<int> findBeginning(RandomAccessFile file, int beginningLength) async {
+  assert(beginningLength > 0);
+
+  final buffer = <int>[];
+  buffer.addAll(await file.read(beginningLength - 1));
+  
+  int i = beginningLength - 1;
+  int b;
+  int removed;
+  final notUniqueElements = <int>[];
+  while ((b = file.readByteSync()) >= 0) {
+    ++i;
+    if (buffer.contains(b)) {
+      notUniqueElements.add(b);
+    }
+    buffer.add(b);
+    if (notUniqueElements.isEmpty) {
+      break;
+    } else {
+      removed = buffer.removeAt(0);
+      notUniqueElements.remove(removed);
+    }
+  }
+  return i;
 }
