058e88bd5b18790434985d4a39fba0dd9d66f074
Anton Kovalyov <keldzh@gmail.com>
2022-12-08 22:13:16 +0300

    Advent Of Code 2022. Day 8: Treetop Tree House. Part 1

--- Main.dart
+++ Main.dart
@@ -1,246 +1,74 @@
 import 'dart:io';
-import 'dart:math';
 
-const FILESYSTEM_MAX_SIZE = 70000000;
-const UPDATE_SIZE = 30000000;
-
-void main() async {
+void main() {
   final input = File("input");
   final inputLines = input.readAsLinesSync();
+  final gridWidth = inputLines.first.length;
+  final gridHeight = inputLines.length;
+  final grid = List<List<Tree>>.generate(gridHeight, (row) {
+    return List<Tree>.generate(
+        gridWidth, (column) => Tree(int.parse(inputLines[row][column])),
+        growable: false);
+  },
+      growable: false);
 
-  final filesystem = DirectoryInfo(null, DirectoryInfo.FILESYSTEM_ROOT_NAME);
+  int k;
+  Tree currentTree;
+  for (int i = 0; i < gridHeight; ++i) {
+    for (int j = 0; j < gridWidth; ++j) {
+      currentTree = grid[i][j];
 
-  Command command;
-  Node currentDirectory = filesystem;
-  while (inputLines.isNotEmpty) {
-    command = Command.fromInputLine(inputLines.removeAt(0));
-    currentDirectory = command.run(currentDirectory, inputLines);
-  }
+      k = j - 1;
+      while (k >= 0 && !currentTree.isHiddenBy(grid[i][k])) --k;
+      currentTree.leftVisible = k < 0;
 
-  printDirectoryRecursive(filesystem, 0);
+      k = j + 1;
+      while (k < gridWidth && !currentTree.isHiddenBy(grid[i][k])) ++k;
+      currentTree.rightVisible = k >= gridWidth;
 
-  final directoriesSizes = <Node, int>{};
-  final totalSize = calculateDirectoriesSizes(filesystem, directoriesSizes);
+      k = i - 1;
+      while (k >= 0 && !currentTree.isHiddenBy(grid[k][j])) --k;
+      currentTree.topVisible = k < 0;
 
-  final needToFree = UPDATE_SIZE - (FILESYSTEM_MAX_SIZE - totalSize);
-  int result = UPDATE_SIZE;
-  Node? directoryToDelete = null;
-  for (final entry in directoriesSizes.entries) {
-    if (entry.value >= needToFree && entry.value < result) {
-      result = entry.value;
-      directoryToDelete = entry.key;
+      k = i + 1;
+      while (k < gridHeight && !currentTree.isHiddenBy(grid[k][j])) ++k;
+      currentTree.bottomVisible = k >= gridHeight;
     }
   }
 
-  print("Total size is $totalSize, need to free $needToFree");
-  print('Remove directory "${directoryToDelete?.name ?? "<unknown>"} with size $result');
-}
+  for (int i = 0; i < gridHeight; ++i) {
+    print(grid[i].join("\t"));
+  }
 
-int calculateDirectoriesSizes(
-    DirectoryInfo directory, Map<Node, int> directoriesSizes) {
   var sum = 0;
-
-  for (final child in directory.children) {
-    if (child is DirectoryInfo) {
-      sum += calculateDirectoriesSizes(child, directoriesSizes);
-    } else if (child is FileInfo) {
-      sum += child.size;
+  for (int i = 0; i < gridHeight; ++i) {
+    for (int j = 0; j < gridWidth; ++j) {
+      if (grid[i][j].isVisible) {
+        sum += 1;
+      }
     }
   }
 
-  directoriesSizes[directory] = sum;
-  return sum;
+  print(sum);
 }
 
-void printDirectoryRecursive(Node currentDirectory, int depth) {
-  var offset = " " * (depth * 2);
-  print("$offset- ${currentDirectory.name} (dir)");
-  offset += "  ";
-  for (final child in currentDirectory.children) {
-    if (child is DirectoryInfo) {
-      printDirectoryRecursive(child, depth + 1);
-    } else {
-      print("$offset- $child");
-    }
+class Tree {
+  final int height;
+  var topVisible = false;
+  var bottomVisible = false;
+  var leftVisible = false;
+  var rightVisible = false;
+
+  get isVisible => topVisible || bottomVisible || leftVisible || rightVisible;
+
+  Tree(this.height);
+
+  bool isHiddenBy(Tree other) {
+    return other.height >= height;
   }
-}
-
-abstract class Node {
-  final String name;
-  final Node? parent;
-  final _children = <Node>[];
-  List<Node> get children => List.unmodifiable(_children);
-
-  Node(this.parent, this.name);
-
-  void put(Node child) {
-    _children.add(child);
-  }
-
-  factory Node.fromInputLine(Node currentDirectory, String inputLine) {
-    if (DirectoryInfo.isDirectory(inputLine)) {
-      return DirectoryInfo.fromInput(currentDirectory, inputLine);
-    } else {
-      return FileInfo.fromInput(currentDirectory, inputLine);
-    }
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Node &&
-          runtimeType == other.runtimeType &&
-          name == other.name &&
-          parent == other.parent;
-
-  @override
-  int get hashCode => name.hashCode ^ parent.hashCode;
-}
-
-class FileInfo extends Node {
-  final int size;
-
-  FileInfo(super.parent, this.size, super.name);
-
-  factory FileInfo.fromInput(Node currentDirectory, String input) {
-    final firstSpaceIndex = input.indexOf(" ");
-    return FileInfo(
-        currentDirectory,
-        int.tryParse(input.substring(0, min(firstSpaceIndex, input.length))) ??
-            0,
-        firstSpaceIndex + 1 < input.length
-            ? input.substring(firstSpaceIndex + 1)
-            : "");
-  }
-
-  @override
-  void put(Node child) {
-    throw UnsupportedError("File cannot have children.");
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is FileInfo &&
-          runtimeType == other.runtimeType &&
-          size == other.size &&
-          name == other.name &&
-          parent == other.parent;
-
-  @override
-  int get hashCode => size.hashCode ^ name.hashCode ^ parent.hashCode;
 
   @override
   String toString() {
-    return 'FileInfo{name: $name, size: $size}';
+    return 'Tree{height: $height, isVisible: $isVisible}';
   }
-}
-
-class DirectoryInfo extends Node {
-  static const _DIRECTORY_PREFIX = "dir ";
-  static const FILESYSTEM_ROOT_NAME = "/";
-  static const PARENT_DIRECTORY = "..";
-
-  DirectoryInfo(super.parent, super.name);
-
-  factory DirectoryInfo.fromInput(Node currentDirectory, String input) {
-    return DirectoryInfo(
-        currentDirectory, input.substring(_DIRECTORY_PREFIX.length));
-  }
-
-  static bool isDirectory(String input) {
-    return input.startsWith(_DIRECTORY_PREFIX);
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      super == other &&
-          other is DirectoryInfo &&
-          runtimeType == other.runtimeType;
-
-  @override
-  int get hashCode => super.hashCode;
-
-  @override
-  String toString() {
-    return 'DirectoryInfo{name: $name, [${children.join(", ")}]}';
-  }
-}
-
-abstract class Command {
-  static const _COMMAND_PREFIX = r"$";
-  final String args;
-
-  Command(this.args);
-
-  Node run(Node currentDirectory, List<String> inputLines);
-
-  static bool isCommand(String inputLine) {
-    return inputLine.startsWith(_COMMAND_PREFIX);
-  }
-
-  factory Command.fromInputLine(String inputLine) {
-    final split = inputLine.split(" ");
-
-    if (split.isEmpty || split.first != _COMMAND_PREFIX) {
-      throw ArgumentError("It isn't command: $inputLine");
-    }
-
-    if (split.length < 2) {
-      throw ArgumentError("Command is missing: $inputLine");
-    }
-
-    switch (split[1]) {
-      case Ls.COMMAND_NAME: return Ls();
-      case Cd.COMMAND_NAME: return Cd(split.skip(2).join(" "));
-      default: throw ArgumentError("Unknown command ${split[1]}.");
-    }
-  }
-}
-
-class Ls extends Command {
-  static const COMMAND_NAME = "ls";
-
-  Ls() : super("");
-
-  @override
-  Node run(Node currentDirectory, List<String> inputLines) {
-    while (inputLines.isNotEmpty && !Command.isCommand(inputLines.first)) {
-      currentDirectory.put(
-          Node.fromInputLine(currentDirectory, inputLines.removeAt(0)));
-    }
-    return currentDirectory;
-  }
-}
-
-class Cd extends Command {
-  static const COMMAND_NAME = "cd";
-
-  Cd(super.args);
-
-  @override
-  Node run(Node currentDirectory, List<String> inputLines) {
-    switch (args) {
-      case DirectoryInfo.FILESYSTEM_ROOT_NAME:
-        Node result = currentDirectory;
-        while (result.parent != null) {
-          result = result.parent!;
-        }
-        return result;
-      case DirectoryInfo.PARENT_DIRECTORY:
-        return currentDirectory.parent ?? currentDirectory;
-      default:
-        return currentDirectory.children.firstWhere((node) => node.name == args,
-            orElse: () =>
-            throw StateError(
-                'Cannot find directory "$args" in current directory: $currentDirectory'));
-    }
-  }
-}
-
-class Container<T> {
-  T value;
-  
-  Container(this.value);
 }
