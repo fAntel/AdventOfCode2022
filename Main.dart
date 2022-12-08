a1b2b5fc9e9b29d4c5f8697a9727258d9f331d79
Anton Kovalyov <keldzh@gmail.com>
2022-12-08 22:29:49 +0300

    Advent Of Code 2022. Day 8: Treetop Tree House. Part 2

--- Main.dart
+++ Main.dart
@@ -21,18 +21,24 @@ void main() {
       k = j - 1;
       while (k >= 0 && !currentTree.isHiddenBy(grid[i][k])) --k;
       currentTree.leftVisible = k < 0;
+      currentTree.leftViewingDistance = currentTree.leftVisible ? j : j - k;
 
       k = j + 1;
       while (k < gridWidth && !currentTree.isHiddenBy(grid[i][k])) ++k;
       currentTree.rightVisible = k >= gridWidth;
+      currentTree.rightViewingDistance =
+          currentTree.rightVisible ? gridWidth - j - 1 : k - j;
 
       k = i - 1;
       while (k >= 0 && !currentTree.isHiddenBy(grid[k][j])) --k;
       currentTree.topVisible = k < 0;
+      currentTree.topViewingDistance = currentTree.topVisible ? i : i - k;
 
       k = i + 1;
       while (k < gridHeight && !currentTree.isHiddenBy(grid[k][j])) ++k;
       currentTree.bottomVisible = k >= gridHeight;
+      currentTree.bottomViewingDistance =
+          currentTree.bottomVisible ? gridHeight - i - 1 : k - i;
     }
   }
 
@@ -40,26 +46,42 @@ void main() {
     print(grid[i].join("\t"));
   }
 
-  var sum = 0;
+  var visibleTreesCount = 0;
+  var maxScenicScore = 0;
   for (int i = 0; i < gridHeight; ++i) {
     for (int j = 0; j < gridWidth; ++j) {
       if (grid[i][j].isVisible) {
-        sum += 1;
+        visibleTreesCount += 1;
+      }
+      if (grid[i][j].scenicScore > maxScenicScore) {
+        maxScenicScore = grid[i][j].scenicScore;
       }
     }
   }
 
-  print(sum);
+  print("Visible trees count: $visibleTreesCount, max scenic score: $maxScenicScore");
 }
 
 class Tree {
   final int height;
+
   var topVisible = false;
   var bottomVisible = false;
   var leftVisible = false;
   var rightVisible = false;
 
+  var topViewingDistance = 0;
+  var bottomViewingDistance = 0;
+  var leftViewingDistance = 0;
+  var rightViewingDistance = 0;
+
   get isVisible => topVisible || bottomVisible || leftVisible || rightVisible;
+
+  get scenicScore =>
+      topViewingDistance *
+      bottomViewingDistance *
+      leftViewingDistance *
+      rightViewingDistance;
 
   Tree(this.height);
 
@@ -69,6 +91,6 @@ class Tree {
 
   @override
   String toString() {
-    return 'Tree{height: $height, isVisible: $isVisible}';
+    return 'Tree{height: $height, isVisible: $isVisible, scenic score: $scenicScore}';
   }
 }
