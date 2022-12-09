a1ae2c3665c4ea4695d547dcfb7863cad4a7393c
Anton Kovalyov <keldzh@gmail.com>
2022-12-10 02:36:05 +0300

    Advent Of Code 2022. Day 9: Rope Bridge. Part 2

--- Main.dart
+++ Main.dart
@@ -4,45 +4,33 @@ import 'dart:math';
 void main() {
   final input = File("input");
   final motions = input.readAsLinesSync().map((e) => Motion.fromInputLine(e));
-
-  var headPosition = Point(0, 0);
-  var tailPosition = Point(0, 0);
-  final tailPositions = {tailPosition};
+  final knots = List<Point<int>>.generate(10, (index) => Point<int>(0, 0));
+  final tailPositions = {knots.last};
 
   for (final motion in motions) {
     for (int i = 0; i < motion.count; ++i) {
       switch (motion.direction) {
         case Direction.left:
-          headPosition = Point(headPosition.x - 1, headPosition.y);
+          knots[0] = Point(knots.first.x - 1, knots.first.y);
           break;
         case Direction.up:
-          headPosition = Point(headPosition.x, headPosition.y - 1);
+          knots[0] = Point(knots.first.x, knots.first.y - 1);
           break;
         case Direction.right:
-          headPosition = Point(headPosition.x + 1, headPosition.y);
+          knots[0] = Point(knots.first.x + 1, knots.first.y);
           break;
         case Direction.down:
-          headPosition = Point(headPosition.x, headPosition.y + 1);
+          knots[0] = Point(knots.first.x, knots.first.y + 1);
           break;
       }
 
-      if (!tailPosition.isTouching(headPosition)) {
-        tailPosition = Point(
-            tailPosition.x + (headPosition.x - tailPosition.x).sign,
-            tailPosition.y + (headPosition.y - tailPosition.y).sign);
-      }
+      knots.pullWholeTail();
 
-      tailPositions.add(tailPosition);
+      tailPositions.add(knots.last);
     }
   }
 
   print(tailPositions.length);
-}
-
-extension PointExtenstions on Point {
-  bool isTouching(Point other) {
-    return (x - other.x).abs() <= 1 && (y - other.y).abs() <= 1;
-  }
 }
 
 class Motion {
@@ -52,8 +40,8 @@ class Motion {
   Motion(this.direction, this.count) : assert(count >= 0);
 
   Motion.fromInputLine(String input) :
-      direction = Direction.fromInput(input),
-      count = int.parse(input.substring(2));
+        direction = Direction.fromInput(input),
+        count = int.parse(input.substring(2));
 }
 
 enum Direction {
@@ -67,6 +55,28 @@ enum Direction {
       case "R": return Direction.right;
       case "D": return Direction.down;
       default: throw ArgumentError("Unknown direction: $directionPart. Full input: $input");
+    }
+  }
+}
+
+extension PointExtenstions on Point<int> {
+  bool isTouching(Point<int> other) {
+    return (x - other.x).abs() <= 1 && (y - other.y).abs() <= 1;
+  }
+
+  Point<int> pull(Point<int> other) {
+    return Point(
+        other.x + (x - other.x).sign,
+        other.y + (y - other.y).sign);
+  }
+}
+
+extension KnotsExtentions on List<Point<int>> {
+  void pullWholeTail() {
+    for (int i = 1; i < length; ++i) {
+      if (!this[i].isTouching(this[i - 1])) {
+        this[i] = this[i - 1].pull(this[i]);
+      }
     }
   }
 }
