ff96b32c8e02c9899a6ec50316939d9488d7bcfb
Anton Kovalyov <keldzh@gmail.com>
2022-12-10 17:12:55 +0300

    Advent Of Code 2022. Day 10: Cathode-Ray Tube. Part 2

--- Main.dart
+++ Main.dart
@@ -1,34 +1,52 @@
 import 'dart:io';
 
+const SCREEN_WIDTH = 40;
+const SCREEN_HEIGHT = 6;
+
 void main() {
   final input = File("input");
   final commands =
       input.readAsLinesSync().map((e) => Command.fromInputLine(e)).toList();
-  final importantCycles =
-      List.generate(6, (index) => index == 0 ? 20 : 20 + index * 40);
+  final screen = List<List<String>>.generate(SCREEN_HEIGHT,
+          (index) => List<String>.generate(SCREEN_WIDTH, (n) => ""));
 
-  var result = 0;
   var cp = 0;
   var x = 1;
-  var currentCycle = 0;
-  for (final nextCycle in importantCycles) {
-    while (cp < commands.length && currentCycle + commands[cp].duration < nextCycle) {
-      x = commands[cp].run(x);
-      currentCycle += commands[cp].duration;
+  for (int currentCycle = 0; currentCycle < SCREEN_HEIGHT * SCREEN_WIDTH; ++currentCycle) {
+    if (x - 1 <= currentCycle % SCREEN_WIDTH && currentCycle % SCREEN_WIDTH <= x + 1) {
+      screen[currentCycle ~/ SCREEN_WIDTH][currentCycle % SCREEN_WIDTH] = "#";
+    } else {
+      screen[currentCycle ~/ SCREEN_WIDTH][currentCycle % SCREEN_WIDTH] = ".";
+    }
+    x = commands[cp].run(x);
+    if (commands[cp].done) {
       ++cp;
     }
-    result += nextCycle * x;
   }
 
-  print(result);
+  for (final row in screen) {
+    print(row.join());
+  }
 }
 
 abstract class Command {
   final int duration;
+  int _alreadyRunning = 0;
+
+  get done => _alreadyRunning >= duration;
 
   Command(this.duration);
 
-  int run(int currentX);
+  int run(int currentX) {
+    _alreadyRunning += 1;
+    if (done) {
+      return _produceResult(currentX);
+    } else {
+      return currentX;
+    }
+  }
+
+  int _produceResult(int currentX);
 
   factory Command.fromInputLine(String inputLine) {
     final parts = inputLine.split(" ");
@@ -46,7 +64,7 @@ class Noop extends Command {
   Noop() : super(1);
 
   @override
-  int run(int currentX) {
+  int _produceResult(int currentX) {
     return currentX;
   }
 }
@@ -59,7 +77,7 @@ class AddX extends Command {
   AddX(this.value) : super(2);
 
   @override
-  int run(int currentX) {
+  int _produceResult(int currentX) {
     return currentX + value;
   }
 }
