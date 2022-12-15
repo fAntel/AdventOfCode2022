76a3911500e463dec15bd088d98b3fec5d0c6491
Anton Kovalyov <keldzh@gmail.com>
2022-12-15 03:56:59 +0300

    Advent Of Code 2022. Day 13: Distress Signal. Part 1

--- Main.dart
+++ Main.dart
@@ -6,233 +6,163 @@ const ROUNDS_COUNT = 10000;
 
 void main() {
   final input = File("input");
-  final List<Monkey> monkeys = input.readAsLinesSync()
+  final pairsOfPackets = input.readAsLinesSync()
       .splitAfter((element) => element.isEmpty)
-      .where((list) => list.isNotEmpty)
-      .map((e) => Monkey.fromInput(e))
+      .map((list) => Pair<Packet, Packet>(
+          Packet.fromInputLine(list.first), Packet.fromInputLine(list[1])))
       .toList();
 
-  final worryReducer = BigIntWorryReducer();
-  for (final Monkey monkey in monkeys)   {
-    for (final Item item in monkey.items) {
-      item.worryReducer = worryReducer;
+  int result = 0;
+  for (int i = 0; i < pairsOfPackets.length; ++i) {
+    if (pairsOfPackets[i].isOrderRight()) {
+      result += i + 1;
     }
   }
 
-  MonkeyInspectionResult inspectionResult;
-  for (int i = 0; i < ROUNDS_COUNT; ++i) {
-    for (final Monkey monkey in monkeys) {
-      while (monkey.hasItemsToInspect) {
-        inspectionResult = monkey.inspectNextItem();
-        monkeys[inspectionResult.otherMonkeyIndex]
-            .catchItem(inspectionResult.itemToThrow);
-      }
-    }
-  }
-
-  final int monkeyBusiness = monkeys
-      .map((monkey) => monkey.itemsInspected)
-      .sorted((a, b) => a.compareTo(b))
-      .skip(monkeys.length - 2)
-      .fold(1, (int acc, int itemsInspected) => acc * itemsInspected);
-  print(monkeyBusiness);
+  print(result);
 }
 
-class Item {
-  BigInt worryLevel;
-  late WorryReducer worryReducer;
+class Pair<F, S> {
+  final F first;
+  final S second;
 
-  Item(this.worryLevel);
-
-  Item.fromInput(String input) : worryLevel = BigInt.parse(input.trim());
-
-  void applyInspection(Operation operation) {
-    worryLevel = worryReducer.reduceWorry(operation.apply(worryLevel));
-  }
+  Pair(F this.first, S this.second);
 
   @override
-  String toString() => 'Item{worryLevel: $worryLevel}';
-}
-
-class Monkey {
-  static const ITEMS_LIST_LINE = "Starting items:";
-  static const OPERATION_LINE = "Operation:";
-  static const TEST_LINE = "Test:";
-  static const TRUE_TEST_RESULT_LINE = "If true:";
-  static const FALSE_TEST_RESULT_LINE = "If false:";
-
-  final List<Item> _items;
-  final Operation _worryLevelChangeOperation;
-  final BigInt divisibleBy;
-  final int _trueMonkeyIndex;
-  final int _falseMonkeyIndex;
-  int _itemsInspected = 0;
-
-  bool get hasItemsToInspect => _items.isNotEmpty;
-  int get itemsInspected => _itemsInspected;
-  List<Item> get items => List.unmodifiable(_items);
-
-  Monkey(this._items, this._worryLevelChangeOperation, int divisibleBy,
-      this._trueMonkeyIndex, this._falseMonkeyIndex)
-      : this.divisibleBy = BigInt.from(divisibleBy);
-
-  MonkeyInspectionResult inspectNextItem() {
-    final item = _items.removeAt(0);
-    item.applyInspection(_worryLevelChangeOperation);
-    int otherMonkeyIndex = item.worryLevel % divisibleBy == BigInt.zero
-        ? _trueMonkeyIndex
-        : _falseMonkeyIndex;
-    _itemsInspected += 1;
-    return MonkeyInspectionResult(otherMonkeyIndex, item);
-  }
-  
-  void catchItem(Item item) {
-    _items.add(item);
-  }
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Pair &&
+              runtimeType == other.runtimeType &&
+              first == other.first &&
+              second == other.second;
 
   @override
-  String toString() {
-    return 'Monkey{_items: ${_items.join(", ")}, _worryLevelChangeOperation: $_worryLevelChangeOperation, _divisibleBy: $divisibleBy, _trueMonkeyIndex: $_trueMonkeyIndex, _falseMonkeyIndex: $_falseMonkeyIndex}';
-  }
+  int get hashCode => first.hashCode ^ second.hashCode;
 
-  factory Monkey.fromInput(List<String> inputLines) {
-    List<Item> items = [];
-    Operation? worryLevelChangeOperation = null;
-    int? divisibleBy = null;
-    int? trueMonkeyIndex = null;
-    int? falseMonkeyIndex = null;
-
-    String inputLine;
-    for (int i = 0; i < inputLines.length; ++i) {
-      inputLine = inputLines[i].trim();
-      if (inputLine.startsWith(ITEMS_LIST_LINE)) {
-        inputLine
-            .substring(ITEMS_LIST_LINE.length)
-            .split(",")
-            .forEach((element) => items.add(Item.fromInput(element)));
-      } else if (inputLine.startsWith(OPERATION_LINE)) {
-        worryLevelChangeOperation = Operation.fromInputLine(inputLine);
-      } else if (inputLine.startsWith(TEST_LINE)) {
-        divisibleBy = int.parse(inputLine.split(" ").last);
-      } else if (inputLine.startsWith(TRUE_TEST_RESULT_LINE)) {
-        trueMonkeyIndex = int.parse(inputLine.split(" ").last);
-      } else if (inputLine.startsWith(FALSE_TEST_RESULT_LINE)) {
-        falseMonkeyIndex = int.parse(inputLine.split(" ").last);
-      }
-    }
-
-    if (items.isNotEmpty &&
-        worryLevelChangeOperation != null &&
-        divisibleBy != null &&
-        trueMonkeyIndex != null &&
-        falseMonkeyIndex != null) {
-      return Monkey(items, worryLevelChangeOperation, divisibleBy,
-          trueMonkeyIndex, falseMonkeyIndex);
-    } else {
-      throw ArgumentError("Not enough data for monkey:\n${inputLines.join("\n")}");
-    }
-  }
+  @override
+  String toString() => 'Pair{first: $first, second: $second}';
 }
 
-abstract class Operation {
-  static const ADD_INPUT_CHARACTER = "+";
-  static const MULTIPLY_AND_DOUBLE_INPUT_CHARACTER = "*";
-  static const DOUBLE_VALUE = "old";
+class Packet {
+  final List<dynamic> data;
 
-  Operation();
+  Packet(this.data);
 
-  BigInt apply(BigInt oldValue);
+  factory Packet.fromInputLine(String inputLine) {
+    final parsedPacket = [];
+    _parseInput(inputLine, parsedPacket, 0);
+    return Packet(parsedPacket);
+  }
 
-  factory Operation.fromInputLine(String inputLine) {
-    final operationData = inputLine.split(" ");
-    if (operationData.length < 2)
-      throw ArgumentError("Cannot parse input, not enough data: $inputLine");
-
-    if (operationData[operationData.length - 2] == ADD_INPUT_CHARACTER) {
-      return Add(BigInt.parse(operationData.last));
-    } else if (operationData[operationData.length - 2] == MULTIPLY_AND_DOUBLE_INPUT_CHARACTER) {
-      if (operationData.last.trim() == DOUBLE_VALUE) {
-        return Double();
+  static int _parseInput(final String input, final List<dynamic> result, int i) {
+    while (i < input.length) {
+      if (input[i] == '[') {
+        var list = [];
+        result.add(list);
+        i = _parseInput(input, list, i + 1);
+      } else if (input[i] == ']') {
+        return i + 1;
+      } else if (int.tryParse(input[i]) != null) {
+        int n = 0;
+        int j = 1;
+        int? parsed = null;
+        do {
+          parsed = int.tryParse(input[i]);
+          if (parsed != null) {
+            n = n * j + parsed;
+            j *= 10;
+            ++i;
+          }
+        } while (i < input.length && parsed != null);
+        result.add(n);
       } else {
-        return Multiply(BigInt.parse(operationData.last));
+        ++i;
       }
-    } else {
-      throw ArgumentError("Unknown operation: $inputLine");
     }
+    return i;
   }
-}
-
-class Add extends Operation {
-  final BigInt _addValue;
-
-  Add(this._addValue);
-
-  @override
-  BigInt apply(BigInt oldValue) => oldValue + _addValue;
-
-  @override
-  String toString() => 'Add{_addValue: $_addValue}';
-}
-
-class Multiply extends Operation {
-  final BigInt _multiplyValue;
-
-  Multiply(this._multiplyValue);
-
-  @override
-  BigInt apply(BigInt oldValue) => oldValue * _multiplyValue;
-
-  @override
-  String toString() => 'Multiply{_multiplyValue: $_multiplyValue}';
-}
-
-class Double extends Operation {
-  Double();
-
-  @override
-  BigInt apply(BigInt oldValue) => oldValue * oldValue;
-
-  @override
-  String toString() => 'Double{}';
-}
-
-abstract class WorryReducer {
-  WorryReducer();
-
-  BigInt reduceWorry(BigInt worryLevel);
-}
-
-class PartOneWorryReducer extends WorryReducer {
-  final BigInt _divider = BigInt.from(3);
-
-  PartOneWorryReducer();
-
-  @override
-  BigInt reduceWorry(BigInt worryLevel) => worryLevel ~/ _divider;
-}
-
-class PartTwoWorryReducer extends WorryReducer {
-  final BigInt _divider;
-
-  PartTwoWorryReducer(this._divider);
-
-  @override
-  BigInt reduceWorry(BigInt worryLevel) => worryLevel % _divider;
-}
-
-class BigIntWorryReducer extends WorryReducer {
-  @override
-  BigInt reduceWorry(BigInt worryLevel) => worryLevel;
-}
-
-class MonkeyInspectionResult {
-  final int otherMonkeyIndex;
-  final Item itemToThrow;
-
-  MonkeyInspectionResult(this.otherMonkeyIndex, this.itemToThrow);
 
   @override
   String toString() {
-    return 'MonkeyInspectionResult{otherMonkeyIndex: $otherMonkeyIndex, itemToThrow: $itemToThrow}';
+    final buf = StringBuffer("Packet{data: ");
+    prepareToString(buf, data);
+    buf.write("}");
+    return buf.toString();
+  }
+
+  void prepareToString(StringBuffer buf, List<dynamic> list) {
+    buf.write("[");
+    for (int i = 0; i < data.length; ++i) {
+      if (data[i] is int) {
+        buf.write(data[i].toString());
+      } else if (data[i] is List) {
+        prepareToString(buf, data[i]);
+      } else {
+        buf.write("${data[i]} (${data[i].runtimeType.toString()})");
+      }
+      if (i + 1 < data.length) {
+        buf.write(", ");
+      }
+    }
+    buf.write("]");
+  }
+}
+
+extension PairOfPacketsExtenstions on Pair<Packet, Packet> {
+  bool isOrderRight() {
+    bool? result = _checkListOrder(first.data, second.data);
+    if (result != null) {
+      return result;
+    } else {
+      throw ArgumentError("Cannot find out order: $this");
+    }
+  }
+
+  bool? _checkListOrder(List<dynamic> first, List<dynamic> second) {
+    int i = 0;
+    dynamic f, s;
+    bool? possibleResult;
+    while (i < first.length && i < second.length) {
+      if (first[i].runtimeType == second[i].runtimeType) {
+        f = first[i];
+        s = second[i];
+      } else {
+        f = first[i] is int ? [first[i]] : first[i];
+        s = second[i] is int ? [second[i]] : second[i];
+      }
+
+      if (f is int) {
+        possibleResult = _checkIntOrder(f, s);
+        if (possibleResult != null) {
+          return possibleResult;
+        }
+      } else if (f is List<dynamic>) {
+        possibleResult = _checkListOrder(f, s);
+        if (possibleResult != null) {
+          return possibleResult;
+        }
+      } else {
+        throw ArgumentError("Unknown type: ${f.runtimeType.toString()}");
+      }
+
+      ++i;
+    }
+    if (first.length < second.length) {
+      return true;
+    } else if (first.length > second.length) {
+      return false;
+    } else {
+      return null;
+    }
+  }
+
+  bool? _checkIntOrder(int first, int second) {
+    if (first < second) {
+      return true;
+    } else if (first > second) {
+      return false;
+    } else {
+      return null;
+    }
   }
 }
